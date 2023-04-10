9d10467ed69a8ed9e6bc987be542b775942353f4
Abraham Paniagua <abraham.paniagua@gmail.com>
2023-04-10 11:43:42 -0700

    Adding book and bookmarking daily progress

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(requests):
-    return render(requests, 'home.html', {'name': 'Bob'})
+    searchTerm = requests.GET.get('searchMovie')
+    return render(requests, 'home.html', {'searchTerm': searchTerm})
 
 def about(requests):
     return HttpResponse('<h1>Welcome to the about page </h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -21,5 +21,6 @@ from movie import views as movieViews
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', movieViews.home),
-    path('about/', movieViews.about)
+    path('about/', movieViews.about),
+    path('signup/', movieViews.signup),
 ]
