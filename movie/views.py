1f95558085073837cc454789e390b46e02f2943b
Abraham Paniagua <abraham.paniagua@gmail.com>
2023-04-17 10:54:00 -0700

    Finished up to chapter 7

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Movie
 
 def home(requests):
-    searchTerm = requests.GET.get('searchMovie')
-    return render(requests, 'home.html', {'searchTerm': searchTerm})
+    searchTerm = requests.GET.get("searchMovie")
+    movies = Movie.objects.all()
+    return render(requests, "home.html", {"searchTerm": searchTerm, "movies": movies})
+
 
 def about(requests):
-    return HttpResponse('<h1>Welcome to the about page </h1>')
+    return HttpResponse("<h1>Welcome to the about page </h1>")
+
 
 def signup(request):
-    email = request.GET.get('email')
-    return render(request, 'signup.html', {'email': email})
+    email = request.GET.get("email")
+    return render(request, "signup.html", {"email": email})
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,10 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', movieViews.home),
-    path('about/', movieViews.about),
-    path('signup/', movieViews.signup),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about, name='about'),
+    path('signup/', movieViews.signup, name='signup'),
+    path('news/', include('news.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+  document_root=settings.MEDIA_ROOT)
